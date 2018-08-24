780e5ac5a37963de43783845c97f0c93d9304aaa
Dantavious Williams <DanFrmSpace@gmail.com>
2018-08-24 02:30:14 -0400

    ESX_Eden_Garage- Edit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# esx_eden_garage
+## ESX_EDEN_GARAGE - Version 1.0
+
+[Official FiveM link] N/A
+
+This is the **latest version** of my edit of ESX_EDEN_GARAGE. I apologize for the low frequency of updates.
+**This is an Edit**
+
+##Requirment:
+* ESX_VehicleShop
+
+**Whats Different?**
+* No markers
+* All One Menu
+
+1)Use the My SQL provided
+2)Put the resource in your server.cfg
+3)Edit the config to add garages or edit the ones I've added.
+
+KNOWN BUGS:
+- You have to be outside of your car to store it.
+- If you exit the menu early you have to leave the area and come back again
+
+I'm working on fixes for both.
+
+
+## Thank you very much
+
+
+If you all wanna join my server it's **Blind Life Roleplay!**
---
+++ config.lua
@@ -0,0 +1,50 @@
+Config = {
+	DrawDistance = 100,
+	Price = 5000,
+	BlipInfos = {
+		Sprite = 290,
+		Color = 5 
+	}
+}
+
+Config.Garages = {
+	Centre = {	
+		Pos = {x=229.2135772705, y=-790.02819824218, z=28.656345367432},
+		Size  = {x = 30.0, y = 30.0, z = -15.0},
+		Color = {r = 255, g = 255, b = 255},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x=229.2135772705, y=-790.02819824218, z=28.656345367432},
+			Color = {r=0,g=0,b=0},
+			Size  = {x = 30.0, y = 30.0, z = -15.0},
+			Marker = 1
+		},
+		DeletePoint = {
+			Pos = {x=229.2135772705, y=-790.02819824218, z=28.656345367432},
+			Color = {r=0,g=0,b=0},
+			Size  = {x = 30.0, y = 30.0, z = -15.0},
+			Marker = 1
+		}, 	
+	},
+	Paleto = {	
+		Pos = {x=105.359, y=6613.586, z=31.3973},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x=128.7822, y= 6622.9965, z= 30.7828},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		},
+		DeletePoint = {
+			Pos = {x=126.3572, y=6608.4150, z=30.8565},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		}, 	
+	},
+
+}
+
+
